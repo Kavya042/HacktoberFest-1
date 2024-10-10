f64c4b1f640cd25b513d3e9ac13727e59698927f
Kavya K Kamath <kavya8310023492@gmail.com>
2024-10-10 18:12:25 +0530

    Update array.java

--- Java Fundamentals programs/array.java
+++ Java Fundamentals programs/array.java
@@ -14,5 +14,17 @@ public class array{
         for (int i=0;i<=myArray.length-1;i++){
             System.out.println(myArray[i]);
         }
+
+        //converting Integer list to int Array
+		List<Integer> list = new ArrayList<>();
+		list.add(3);
+		list.add(90);
+		list.add(80);
+		System.out.println(list);
+		
+		Object[] arr=list.toArray();
+		for(Object obj:arr) {
+			System.out.println(obj);
+		}
     }
 }
